3bb121fd569016908c34d96d7f9e5df46566c88a
alicerestituta <alicerestituta@gmail.com>
2026-05-25 11:57:09 +0700

    Rusak kode untuk uji coba\

--- math.js
+++ math.js
@@ -1,4 +1,5 @@
+// math.js
 function tambah(a, b) {
-  return a + b;
+  return a + b + 1; // Sengaja ditambah 1 supaya hasilnya salah (1+2 seharusnya 3, tapi jadi 4)
 }
 module.exports = { tambah };
